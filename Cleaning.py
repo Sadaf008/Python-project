93113fde4dd12363f74685fdd3dfbb96faeddaf5
Sadaf Hafeez <sadafwattoo944@gmail.com>
2026-03-08 20:50:51 +0500

    Delete Cleaning.py

--- Cleaning.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("D:\\Python\\archive\\netflix_titles.csv", encoding="latin-1")
-print(df.head())
-print(df.info())
-print(df.isnull().sum())
-#Data Cleaning
-df.drop_duplicates(inplace=True)
-df.dropna(subset=['country', 'type', 'release_year', 'listed_in'], inplace=True)
-
